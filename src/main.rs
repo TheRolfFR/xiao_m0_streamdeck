1a89634f9b69e8a1f7ed1eda263201ccc21e9bce
Yann Le Vagueres <yann.lv@live.fr>
2025-05-22 20:50:49 +0200

    Fixed descriptor a little bit

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,9 @@ use panic_halt as _;
 
 use hal::{clock::GenericClockController};
 use pac::{Peripherals};
-use usb_device::bus::InterfaceNumber;
-use usb_device::bus::UsbBusAllocator;
-use usb_device::class::UsbClass;
-use usb_device::descriptor::DescriptorWriter;
-use usb_device::endpoint::EndpointIn;
-use usb_device::endpoint::EndpointOut;
 use usbd_hid::hid_class::HIDClass;
+use xiao_m0::hal::prelude::_atsamd_hal_embedded_hal_digital_v2_ToggleableOutputPin;
+use xiao_m0::Led1;
 use xiao_m0 as bsp;
 
 use bsp::{hal, pac};
@@ -21,57 +17,23 @@ use usb_device::prelude::*;
 use usbd_hid::descriptor::gen_hid_descriptor;
 use usbd_hid::descriptor::generator_prelude::*;
 
-pub struct StreamDeckClass<'a, B: usb_device::bus::UsbBus> {
-    interface: InterfaceNumber,
-    write_ep: EndpointIn<'a, B>,
-    read_ep: EndpointOut<'a, B>,
-}
-
-impl<B: usb_device::bus::UsbBus> StreamDeckClass<'_, B> {
-    pub fn new(alloc: &UsbBusAllocator<B>) -> StreamDeckClass<B> {
-        StreamDeckClass {
-            interface: alloc.interface(),
-            write_ep: alloc.bulk(64),
-            read_ep: alloc.bulk(64),
-        }
-    }
-
-    pub fn poll(&mut self) {
-        let mut buf = [0u8; 64];
-        if let Ok(count) = self.read_ep.read(&mut buf) {
-            // Echo back the received bytes (or handle Stream Deck protocol)
-            let _ = self.write_ep.write(&buf[0..count]);
-        }
-    }
-}
-
-impl<B: usb_device::bus::UsbBus> UsbClass<B> for StreamDeckClass<'_, B> {
-    fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> usb_device::Result<()> {
-        writer.interface(
-            self.interface,
-            0xFF, // Vendor-specific class
-            0x00, // Subclass
-            0x00, // Protocol
-        )?;
-        writer.endpoint(&self.read_ep)?;
-        writer.endpoint(&self.write_ep)?;
-        Ok(())
-    }
-}
-
 #[gen_hid_descriptor(
     (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = KEYBOARD) = {
-        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0xFF) = {
-            #[item_settings data, variable, absolute] inside=input
-        };
-        (usage_page = KEYBOARD, usage_min = 0x00, usage_max = 0xFF) = {
-            #[item_settings data, variable, absolute] outside=output
+        (usage = 0x00, report_id=0x2,) = {
+            #[item_settings data, array, absolute] payload=output // Output (Host to Device)
         };
     }
 )]
 pub struct StreamDeckReport {
-    pub inside: u8,
-    pub outside: u8
+    pub payload: [u8; 1024],
+}
+
+impl Default for StreamDeckReport {
+    fn default() -> Self {
+        Self {
+            payload: [0u8; 1024],
+        }
+    }
 }
 
 #[entry]
@@ -87,6 +49,8 @@ fn main() -> ! {
 
     let pins = bsp::pins::Pins::new(peripherals.PORT);
 
+    let mut led_pin: Led1 = pins.led1.into_mode();
+
     let bus_allocator = bsp::usb_allocator(
         peripherals.USB,
         &mut clocks,
@@ -94,8 +58,6 @@ fn main() -> ! {
         pins.usb_dm,
         pins.usb_dp,
     );
-
-    let mut streamdeck = StreamDeckClass::new(&bus_allocator);
 
     let mut hid = HIDClass::new(&bus_allocator, StreamDeckReport::desc(), 60);
 
@@ -110,9 +72,19 @@ fn main() -> ! {
         ]).unwrap()
         .build();
 
+        let mut buf = [0u8; 1024];
+
         loop {
-            if usb_dev.poll(&mut [&mut streamdeck, &mut hid]) {
-                streamdeck.poll();
+            if usb_dev.poll(&mut [&mut hid]) {
+                if let Ok(count) = hid.pull_raw_output(&mut buf) {
+                    led_pin.toggle().ok();
+                    if count > 0 && buf[0] == 0x02 {
+
+                        // Send the response back to the host
+                        let mut report = StreamDeckReport::default();
+                        report.payload[0] = count as u8;
+                    }
+                }
             }
         }
 }
